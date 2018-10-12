01f559543682109fb54cf6755ec0610e44958c99
Anthony Ramirez <ar4477@nyu.edu>
2018-10-11 21:53:06 -0400

    add more css, add ‘add’ template, rearrange other templates and fix reverse array bug in app

--- app.js
+++ app.js
@@ -7,14 +7,29 @@ const app = express();
 
 // array of artwork objects to be displayed in reverse order
 let artworkStrings = [
-  {title: "washington sq arch", date: "2018-09-29", artwork: ` _______________\n |~|_________|~|\n |::::\\^o^/::::|\n ---------------\n |..|/     \\|..| \n ---        ----\n |  |       |  |\n |  |       |  |\n |  |       |  |\n.|__|.     .|__|.`, tags: ['architecture', 'public']}, {title: "boba", date: "2018-09-30", artwork: "   ______" + `
-  ======\n
- /      \\n
-|        |-.\n
-|        |  \\n
-|O.o:.o8o|_ /\n
-|.o.8o.O.|\n
- \.o:o.o/`, tags: ['snack', 'notmybestwork']}, {title: "buddy", date: "2018-10-31", artwork: "testing", tags: ['halloween', 'squad', 'fashion']}];
+  {title: "washington sq arch", date: "2018-09-29", artwork:
+  `  _______________
+  |~|_________|~|
+  |::::\\^o^/::::|
+  ---------------
+  |..|/     \\|..|
+  ----       ----
+  |  |       |  |
+  |  |       |  |
+  |  |       |  |
+ .|__|.     .|__|.`, tags: ['architecture', 'public']},
+ {title: "boba", date: "2018-09-30", artwork:
+ `  ======
+ /      \\
+|        |-.
+|        |  \\
+|O.o:.o8o|_ /
+|.o.8o.O.|
+ \\.o:o.o/`, tags: ['snack', 'notmybestwork']},
+ {title: "buddy", date: "2018-10-31", artwork:
+  "      /  /\\   |---.\   \n      |__|/__ |---,\\\n      |  `   |=    `\n      |      /|\n      |  .--' |\n      |   |\\  |\n      |   | \\ |\n     /|   | | |\n    \\/    |  \\|\n___ /_____\\___|\\____", tags: ['halloween', 'squad', 'fashion']}];
+ // reverse array so most recent art displays first
+ artworkStrings.reverse();
 
 // set up handlebars
 app.set('view engine', 'hbs');
@@ -36,14 +51,14 @@ app.use((req, res, next) => {
 
 // homepage
 app.get('/', (req, res) => {
+  // get query string from url
   let qString = req.query.tag;
   let context = '';
+
   // get context to array of artwork objects to be used as variable
   // in homepage.hbs (each artwork object will be its own li element)
   if (!qString) {
-    // if no filter is chosen, reverse artwork array and
     // let every image be part of what to display
-    artworkStrings.reverse();
     context = {artWork: artworkStrings};
   }
   else {
@@ -51,18 +66,19 @@ app.get('/', (req, res) => {
     // so that artwork with only the specified tag shows up
     context = {artWork: artworkStrings.filter(art => art.tags.includes(qString))};
   }
+
   // render homepage with the appropriate artwork
   res.render('homepage', context);
 });
 
 app.get('/add', (req, res) => {
   // add page has a form so that new pieces of art work can be added to array - each input has name that corresponds to artwork object properties
-  res.send('<form method="post" action="/add"><input type="text" name="title" value="title"><input type="text" name="dt" value="dt"><input type="text" name="artwork" value="artwork"><input type="text" name="tags" value="tags"><input type="submit" value="add artwork"></form>');
+  res.render('add');
 });
 
 app.post('/add', (req, res) => {
-  // add new pieces of art work to array
-  artworkStrings.push({title: req.body.title, date: req.body.dt, artwork: req.body.artwork, tags: req.body.tags});
+  // add new pieces of art work to front of array
+  artworkStrings.unshift({title: req.body.title, date: req.body.dt, artwork: req.body.artwork, tags: req.body.tags.split(' ')});
   // redirect back to homepage
   res.redirect('/');
 });
